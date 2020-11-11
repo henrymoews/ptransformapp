0e34f8959e7b1eaadefa77c39e6cefa671029778
Vanessa Pertsch <pertsch@kontrollfeld.de>
2020-11-11 10:31:36 +0100

    move menu from App to components folder.

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css'
 import PTMap from './map/PTMap'
 import { STEPS } from './recording/Steps'
@@ -7,12 +9,10 @@ import MapOverlay from './map/MapOverlay'
 import Button from '@material-ui/core/Button'
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
 import { makeStyles } from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import MenuIcon from '@material-ui/icons/Menu'
+
 import Recording from './recording/Recording'
+import MainMenu from './components/MainMenu'
+import Welcome from './components/Welcome'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,11 +31,11 @@ const APP_STATE = Object.freeze({
   RECORDING: Symbol('RECORDING')
 })
 
-function App () {
+function App() {
 
   const [appState, setAppState] = useState(APP_STATE.RECORDING)
 
-  function renderAppState () {
+  function renderAppState() {
     switch (appState) {
       case APP_STATE.RECORDING:
         return <Recording />
@@ -48,18 +48,20 @@ function App () {
   const classes = useStyles()
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon/>
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            ParkplatzTransform
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      {renderAppState()}
+      <Router>
+        <Route path='/' component={MainMenu} />
+
+        <Route path='/welcome' component={Welcome} />
+
+        <Route exact path='/'>
+          {renderAppState()}
+        </Route>
+
+
+
+
+      </Router>
+
     </div>
   )
 }
