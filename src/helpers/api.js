279963f990e2547850d2d9b404ea8b9f45954c23
Henry Moews <henry.moews@equeo.de>
2020-12-09 12:51:08 +0100

    added two api calls

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -10,3 +10,13 @@ export const headers = {
   contentJSON: new Headers({ 'Content-Type': 'application/json' })
 }
 
+export async function postSegment(segment) {
+  console.log('Not yet implemented: postSegments')
+  const segmentToReturn = Object.assign({}, segment)
+  segmentToReturn.id  = Math.floor(Math.random() * 1000)
+  return segmentToReturn
+}
+
+export async function deleteSegments(segmentId) {
+  console.log('Not yet implemented: deleteSegments')
+}
