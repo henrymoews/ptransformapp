30ffa4a257d4bbaec930548a2281b542a56a4336
Vanessa Pertsch <pertsch@kontrollfeld.de>
2020-12-29 10:56:36 +0100

    #add redirect from /home to /

--- src/app/App.js
+++ src/app/App.js
@@ -1,47 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import './App.css'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Redirect } from "react-router";
+import "./App.css";
 
-import Recording from './recording/Recording'
-import MainMenu from './components/MainMenu'
-import Welcome from './components/Welcome'
-import VerifyToken from './components/VerifyToken'
+import Recording from "./recording/Recording";
+import MainMenu from "./components/MainMenu";
+import Welcome from "./components/Welcome";
+import VerifyToken from "./components/VerifyToken";
 
 const APP_STATE = Object.freeze({
-  RECORDING: Symbol('RECORDING')
-})
+  RECORDING: Symbol("RECORDING"),
+});
 
-function App () {
+function App() {
+  const [appState, setAppState] = useState(APP_STATE.RECORDING);
 
-  const [appState, setAppState] = useState(APP_STATE.RECORDING)
-
-  function renderAppState () {
+  function renderAppState() {
     switch (appState) {
       case APP_STATE.RECORDING:
-        return <Recording/>
+        return <Recording />;
 
       default:
-        return 'Not yet implemented'
+        return "Not yet implemented";
     }
   }
 
   return (
     <div>
       <Router>
-        <Route path='/' component={MainMenu}/>
-
-        <Route path='/welcome' component={Welcome}/>
-        <Route path='/verify-token' component={VerifyToken} />
-
-        <Route exact path='/'>
+        <Route path="/" component={MainMenu} />
+        <Route exact path="/home">
+          <Redirect to="/" />
+        </Route>
+        <Route path="/welcome" component={Welcome} />
+        <Route path="/verify-token" component={VerifyToken} />
+        <Route exact path="/">
           {renderAppState()}
         </Route>
-
       </Router>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
