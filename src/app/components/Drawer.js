bf351d85796048a1123bd16c8a610a5ec1b1e529
Vanessa Pertsch <pertsch@kontrollfeld.de>
2020-12-29 10:55:50 +0100

    #11 add Drawer component. remove menu from MainMenu component

---
+++ src/app/components/Drawer.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import LoginForm from "./LoginForm";
+import { getUserDataFromCookie } from "../../helpers/auth";
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
+  },
+  link: {
+    fontSize: "1.2rem",
+  },
+});
+
+export default function TemporaryDrawer() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Home", "Welcome", "About"].map((text, index) => (
+          <ListItem button key={text}>
+            <Link className={classes.link} to={text}>
+              <ListItemText primary={text} />
+            </Link>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <div>
+      {["left"].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            onClick={toggleDrawer(anchor, true)}
+          >
+            {" "}
+            <MenuIcon />
+          </IconButton>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
--- src/app/components/MainMenu.js
+++ src/app/components/MainMenu.js
@@ -1,19 +1,16 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
 
-import './components.css'
-import { makeStyles } from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import MenuIcon from '@material-ui/icons/Menu'
-import Menu from '@material-ui/core/Menu'
-import MenuItem from '@material-ui/core/MenuItem'
-import LoginForm from './LoginForm'
-import { getUserDataFromCookie } from '../../helpers/auth'
+import "./components.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 
+import Typography from "@material-ui/core/Typography";
+
+import LoginForm from "./LoginForm";
+import { getUserDataFromCookie } from "../../helpers/auth";
+import TemporaryDrawer from "./Drawer";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,15 +22,13 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
-}))
-
-
+}));
 
 function MainMenu() {
-  const classes = useStyles()
-  const [anchorEl, setAnchorEl] = React.useState(null)
-  const [loginModalOpen, setLoginModalOpen] = React.useState(false)
-  const userData = getUserDataFromCookie()
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [loginModalOpen, setLoginModalOpen] = React.useState(false);
+  const userData = getUserDataFromCookie();
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -46,33 +41,17 @@ function MainMenu() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-          <MenuIcon />
-        </IconButton>
+        <TemporaryDrawer />
         <LoginForm open={loginModalOpen} setOpen={setLoginModalOpen} />
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose}>
-          <NavLink  activeClassName="is-active" to="/">
-            Home
-          </NavLink>
-          </MenuItem>
-          <MenuItem onClick={handleClose}><NavLink activeClassName="is-active" to="/welcome">
-            Welcome
-            </NavLink></MenuItem>
-          <MenuItem onClick={handleClose}><NavLink activeClassName="is-active" to="/about">
-           About
-            </NavLink></MenuItem>
-        </Menu>
+
         <Typography variant="h6" className={classes.title}>
           ParkplatzTransform
         </Typography>
-        {!userData.loggedIn && <Button color="inherit" onClick={() => setLoginModalOpen(true)}>Login</Button>}
+        {!userData.loggedIn && (
+          <Button color="inherit" onClick={() => setLoginModalOpen(true)}>
+            Login
+          </Button>
+        )}
         {userData.loggedIn && userData.email}
       </Toolbar>
     </AppBar>
